889368dcdc3cbebcb87c00dd4d577819c9c356f5
GuHongQuan <guhongquan@ones.ai>
2023-11-14 14:31:56 +0800

    remove: some field

--- blueocean.go
+++ blueocean.go
@@ -7,39 +7,13 @@ import (
 )
 
 type BlueOceanStage struct {
-	Class string `json:"_class"`
-	Links struct {
-		Self struct {
-			Class string `json:"_class"`
-			Href  string `json:"href"`
-		} `json:"self"`
-		Actions struct {
-			Class string `json:"_class"`
-			Href  string `json:"href"`
-		} `json:"actions"`
-		Steps struct {
-			Class string `json:"_class"`
-			Href  string `json:"href"`
-		} `json:"steps"`
-	} `json:"_links"`
-	Actions            []interface{} `json:"actions"`
-	DisplayDescription interface{}   `json:"displayDescription"`
-	DisplayName        string        `json:"displayName"`
-	DurationInMillis   int           `json:"durationInMillis"`
-	Id                 string        `json:"id"`
-	Input              interface{}   `json:"input"`
-	Result             string        `json:"result"`
-	StartTime          string        `json:"startTime"`
-	State              string        `json:"state"`
-	Type               string        `json:"type"`
-	CauseOfBlockage    interface{}   `json:"causeOfBlockage"`
-	Edges              []struct {
-		Class string `json:"_class"`
-		Id    string `json:"id"`
-		Type  string `json:"type"`
-	} `json:"edges"`
+	DisplayName string      `json:"displayName"`
+	Id          string      `json:"id"`
+	Input       interface{} `json:"input"`
+	Result      string      `json:"result"`
+	State       string      `json:"state"`
+	Type        string      `json:"type"`
 	FirstParent interface{} `json:"firstParent"`
-	Restartable bool        `json:"restartable"`
 }
 
 func (j *Job) GetBlueOceanRunStages(ctx context.Context, number int) ([]*BlueOceanStage, error) {
